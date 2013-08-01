f6b6e428f945b6b657b0b54daa15eac294c1059e
Neville Davey <nevilledavey@gmail.com>
2013-08-01 15:34:04 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agentinsurance::Application.config.secret_token = '822ca08a16a5f352470b9d59834bfd5abe2f4782c6251adad2a26326271f5329a86fb82d4862a71c7452ae2440f6e5190490cd5717ac227b3a496fb12f8e2087'
